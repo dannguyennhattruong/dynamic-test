5a5cc41c5509c203155621905479bd6776af7f1a
truongdnn <nhattruong2513@gmail.com>
2026-01-19 16:22:47 +0700

    save

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -6,15 +6,32 @@ import {
   mainnet,
   optimism,
   polygon,
+  sepolia,
+  bsc,
+  tron,
 } from "wagmi/chains";
 
-const chains = [mainnet, polygon, arbitrum, optimism, base, avalanche] as const;
+const chains = [mainnet, polygon, arbitrum, optimism, base, avalanche, tron] as const;
 
+// export const config = createConfig({
+//   chains,
+//   multiInjectedProviderDiscovery: false,
+//   ssr: true,
+//   transports: {
+//     [mainnet.id]: http(),
+//     [polygon.id]: http(),
+//     [arbitrum.id]: http(),
+//     [optimism.id]: http(),
+//     [base.id]: http(),
+//     [avalanche.id]: http(),
+//   },
+// });
 export const config = createConfig({
   chains,
   multiInjectedProviderDiscovery: false,
   ssr: true,
   transports: {
+    [tron.id]: http(),
     [mainnet.id]: http(),
     [polygon.id]: http(),
     [arbitrum.id]: http(),
